4575c2087f67eab5d1eaaf4233b4f5b82501243b
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-04-17 21:35:05 +0430

    add word data class

---
+++ app/src/main/java/com/example/hw14/Word.kt
@@ -0,0 +1,14 @@
+package com.example.hw14
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo val wordTitle: String,
+    @ColumnInfo val meaning: String,
+    @ColumnInfo val example: String,
+    @ColumnInfo val synonyms: String,
+)
