0164b1ad64fb0ab2bbd95be6e09c2965a45a7cf7
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-04-19 15:46:43 +0430

    add internet permission to manifest

--- app/src/main/java/com/example/hw14/view/WebViewFragment.kt
+++ app/src/main/java/com/example/hw14/view/WebViewFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.hw14.R
+import com.example.hw14.databinding.FragmentSearchWordBinding
+import com.example.hw14.viewmodel.WordViewModel
 
 
 class WebViewFragment : Fragment() {
-
+    private lateinit var binding: FragmentSearchWordBinding
+    val wordViewModel: WordViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,5 +27,8 @@ class WebViewFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_web_view, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
 }
