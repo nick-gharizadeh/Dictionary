d7e044a9e5b66dd6c1178112619f42dac54ebd85
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-04-23 23:40:13 +0430

    implement favorite recycler view

---
+++ app/src/main/java/com/example/hw14/adaptor/FavWordAdaptor.kt
@@ -0,0 +1,54 @@
+package com.example.hw14.adaptor
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hw14.R
+import com.example.hw14.databinding.ItemBinding
+import com.example.hw14.databinding.ItemfavBinding
+import com.example.hw14.model.Word
+
+
+typealias FavClickHandler = (Word) -> Unit
+
+class FavWordAdaptor(var clickHandler: FavClickHandler) : ListAdapter<Word, FavWordAdaptor.ItemHolder>(FavWordDiffCallback) {
+
+    class ItemHolder(val binding: ItemfavBinding): RecyclerView.ViewHolder(binding.root)
+
+    object FavWordDiffCallback : DiffUtil.ItemCallback<Word>() {
+
+        override fun areItemsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Word, newItem: Word): Boolean {
+            return oldItem.id == newItem.id
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val binding: ItemfavBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.itemfav,
+            parent,
+            false
+        )
+        return ItemHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        holder.binding.word = getItem(position)
+        holder.binding.textViewWordItemFav.setOnClickListener {
+            clickHandler.invoke(getItem(position))
+        }
+        holder.binding.textViewMeaningItemFav.setOnClickListener {
+            clickHandler.invoke(getItem(position))
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/hw14/view/FavFragment.kt
+++ app/src/main/java/com/example/hw14/view/FavFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.hw14.R
+import com.example.hw14.databinding.FragmentFavBinding
+import com.example.hw14.databinding.FragmentInsertWordBinding
+import com.example.hw14.databinding.FragmentSearchWordBinding
+import com.example.hw14.viewmodel.WordViewModel
 
 
 class FavFragment : Fragment() {
-
+    private lateinit var binding: FragmentFavBinding
+    val wordViewModel: WordViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,7 +25,12 @@ class FavFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_fav, container, false)
+        binding = FragmentFavBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
